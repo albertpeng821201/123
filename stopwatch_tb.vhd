733c43201e4543d72862262f7212095aa4a91d81
albertpeng821201 <50378196+albertpeng821201@users.noreply.github.com>
2019-07-10 14:58:23 +0200

    Add files via upload

--- stopwatch_tb.vhd
+++ stopwatch_tb.vhd
@@ -1,3 +1,30 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   14:44:28 07/10/2019
+-- Design Name:   
+-- Module Name:   /nas/ei/home/ge73gej/Desktop/stopwatch/test/stopwatch_tb.vhd
+-- Project Name:  test
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: stop_watch
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
@@ -15,7 +42,9 @@ ARCHITECTURE behavior OF stopwatch_tb IS
     COMPONENT stop_watch
     PORT(
          clk_to_100hz : IN  std_logic;
+         clk : IN  std_logic;
          reset : IN  std_logic;
+         mode_sw : IN  std_logic;
          action_imp : IN  std_logic;
          mode_imp : IN  std_logic;
          minus_imp : IN  std_logic;
@@ -24,14 +53,17 @@ ARCHITECTURE behavior OF stopwatch_tb IS
          csec_count : OUT  std_logic_vector(7 downto 0);
          sec_count : OUT  std_logic_vector(7 downto 0);
          min_count : OUT  std_logic_vector(7 downto 0);
-         hour_count : OUT  std_logic_vector(7 downto 0)
+         hour_count : OUT  std_logic_vector(7 downto 0);
+         freeze_lap : OUT  std_logic
         );
     END COMPONENT;
     
 
    --Inputs
    signal clk_to_100hz : std_logic := '0';
+   signal clk : std_logic := '0';
    signal reset : std_logic := '0';
+   signal mode_sw : std_logic := '0';
    signal action_imp : std_logic := '0';
    signal mode_imp : std_logic := '0';
    signal minus_imp : std_logic := '0';
@@ -43,21 +75,20 @@ ARCHITECTURE behavior OF stopwatch_tb IS
    signal sec_count : std_logic_vector(7 downto 0);
    signal min_count : std_logic_vector(7 downto 0);
    signal hour_count : std_logic_vector(7 downto 0);
--------------------------
+   signal freeze_lap : std_logic;
 
-
-  -- Clock period definitions
-	
-	
-	
-   constant clk_to_100hz_period : time := 10ms;
+   -- Clock period definitions
+   constant clk_to_100hz_period : time := 10 ms;
+   constant clk_period : time := 10ms;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: stop_watch PORT MAP (
           clk_to_100hz => clk_to_100hz,
+          clk => clk,
           reset => reset,
+          mode_sw => mode_sw,
           action_imp => action_imp,
           mode_imp => mode_imp,
           minus_imp => minus_imp,
@@ -66,7 +97,8 @@ BEGIN
           csec_count => csec_count,
           sec_count => sec_count,
           min_count => min_count,
-          hour_count => hour_count
+          hour_count => hour_count,
+          freeze_lap => freeze_lap
         );
 
    -- Clock process definitions
@@ -78,144 +110,153 @@ BEGIN
 		wait for clk_to_100hz_period/2;
    end process;
  
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
 
+   -- Stimulus process
    -- Stimulus process
    stim_proc: process
 	begin
 		wait for 10ms;	
-
+		mode_sw <= '1';
       --wait for clk_to_100hz_period*10;
 
       -- insert stimulus here 
 		
---test into stop watch display
-		wait for 10ms;
-		plus_minus	<= '1'; -- into stop watch display
-		wait for 10ms;
-		plus_minus	<= '0';
-
-		wait for 10ms;
-		reset	<= '1';	--reset
-		wait for 10ms;
-		reset	<= '0';
---test into counter start mode
-		wait for 10ms;
-		plus_minus	<= '1';	-- into stop watch display
-		wait for 10ms;
-		plus_minus	<= '0';
-
-		wait for 10ms;
-		action_imp	<= '1';	-- into counter start mode
-		wait for 10ms;
-		action_imp	<= '0';
-
-		wait for 10ms;
-
-		wait for 10ms;
-		plus_imp	<= '1';	-- back to stop watch display
-		wait for 10ms;
-		plus_imp	<= '0';
-
-		wait for 10ms;
-		action_imp	<= '1';	-- into counter start mode
-		wait for 10ms;
-		action_imp	<= '0';
-
-		wait for 10ms;
-
-		wait for 10ms;
-		reset	<= '1';	--reset
-		wait for 10ms;
-		reset	<= '0';
-
--- test when counting back to time display mode
-		
-		wait for 10ms;
-		plus_minus	<= '1';	-- into stop watch display
-		wait for 10ms;
-		plus_minus	<= '0';
-
-		wait for 10ms;
-		action_imp	<= '1';	-- into counter start mode
-		wait for 10ms;
-		action_imp	<= '0';
-
-		wait for 10ms;
-
-		wait for 10ms;
-		mode_imp	<= '1';	-- into display mode
-		wait for 10ms;
-		mode_imp	<= '0';
-
-		wait for 10ms;
-		plus_minus	<= '1';	-- into stop watch display
-		wait for 10ms;
-		plus_minus	<= '0';
-
-		wait for 10ms;
-
-		wait for 10ms;
-		mode_imp	<= '1';	-- into display mode
-		wait for 10ms;
-		mode_imp	<= '0';
-
-		wait for 10ms;
-		reset	<= '1';	--reset
-		wait for 10ms;
-		reset	<= '0';
-
---test into counter stop mode
-
-		wait for 10ms;
-		plus_minus	<= '1';	-- into stop watch display
-		wait for 10ms;
-		plus_minus	<= '0';
-
-		wait for 10ms;
-		action_imp	<= '1';	-- into counter start mode
-		wait for 10ms;
-		action_imp	<= '0';
-
-		wait for 10ms;
-
-		wait for 10ms;
-		action_imp	<= '1';	-- into counter stop mode
-		wait for 10ms;
-		action_imp	<= '0';
-
-		wait for 10ms;
-
-		wait for 10ms;
-		action_imp	<= '1';	-- back counter start mode
-		wait for 10ms;
-		action_imp	<= '0';
-
-		wait for 10ms;
-
-		wait for 10ms;
-		action_imp	<= '1';	-- into counter stop mode
-		wait for 10ms;
-		action_imp	<= '0';
-
-		wait for 10ms;
-		plus_imp	<= '1';	-- into stop watch display
-		wait for 10ms;
-		plus_imp	<= '0';
-
-		wait for 10ms;
-		action_imp	<= '1';	-- into counter start mode
-		wait for 10ms;
-		action_imp	<= '0';
-
-		wait for 10ms;
-		action_imp	<= '1';	-- into counter stop mode
-		wait for 10ms;
-		action_imp	<= '0';
-
-		wait for 10ms;
-		reset	<= '1';	--reset
-		wait for 10ms;
-		reset	<= '0';	
+----test into stop watch display
+--		wait for 10ms;
+--		plus_minus	<= '1'; -- into stop watch display
+--		wait for 10ms;
+--		plus_minus	<= '0';
+--
+--		wait for 10ms;
+--		reset	<= '1';	--reset
+--		wait for 10ms;
+--		reset	<= '0';
+----test into counter start mode
+--		wait for 10ms;
+--		plus_minus	<= '1';	-- into stop watch display
+--		wait for 10ms;
+--		plus_minus	<= '0';
+--
+--		wait for 10ms;
+--		action_imp	<= '1';	-- into counter start mode
+--		wait for 10ms;
+--		action_imp	<= '0';
+--
+--		wait for 10ms;
+--
+--		wait for 10ms;
+--		plus_imp	<= '1';	-- back to stop watch display
+--		wait for 10ms;
+--		plus_imp	<= '0';
+--
+--		wait for 10ms;
+--		action_imp	<= '1';	-- into counter start mode
+--		wait for 10ms;
+--		action_imp	<= '0';
+--
+--		wait for 10ms;
+--
+--		wait for 10ms;
+--		reset	<= '1';	--reset
+--		wait for 10ms;
+--		reset	<= '0';
+--
+---- test when counting back to time display mode
+--		
+--		wait for 10ms;
+--		plus_minus	<= '1';	-- into stop watch display
+--		wait for 10ms;
+--		plus_minus	<= '0';
+--
+--		wait for 10ms;
+--		action_imp	<= '1';	-- into counter start mode
+--		wait for 10ms;
+--		action_imp	<= '0';
+--
+--		wait for 10ms;
+--
+--		wait for 10ms;
+--		mode_imp	<= '1';	-- into display mode
+--		wait for 10ms;
+--		mode_imp	<= '0';
+--
+--		wait for 10ms;
+--		plus_minus	<= '1';	-- into stop watch display
+--		wait for 10ms;
+--		plus_minus	<= '0';
+--
+--		wait for 10ms;
+--
+--		wait for 10ms;
+--		mode_imp	<= '1';	-- into display mode
+--		wait for 10ms;
+--		mode_imp	<= '0';
+--
+--		wait for 10ms;
+--		reset	<= '1';	--reset
+--		wait for 10ms;
+--		reset	<= '0';
+--
+----test into counter stop mode
+--
+--		wait for 10ms;
+--		plus_minus	<= '1';	-- into stop watch display
+--		wait for 10ms;
+--		plus_minus	<= '0';
+--
+--		wait for 10ms;
+--		action_imp	<= '1';	-- into counter start mode
+--		wait for 10ms;
+--		action_imp	<= '0';
+--
+--		wait for 10ms;
+--
+--		wait for 10ms;
+--		action_imp	<= '1';	-- into counter stop mode
+--		wait for 10ms;
+--		action_imp	<= '0';
+--
+--		wait for 10ms;
+--
+--		wait for 10ms;
+--		action_imp	<= '1';	-- back counter start mode
+--		wait for 10ms;
+--		action_imp	<= '0';
+--
+--		wait for 10ms;
+--
+--		wait for 10ms;
+--		action_imp	<= '1';	-- into counter stop mode
+--		wait for 10ms;
+--		action_imp	<= '0';
+--
+--		wait for 10ms;
+--		plus_imp	<= '1';	-- into stop watch display
+--		wait for 10ms;
+--		plus_imp	<= '0';
+--
+--		wait for 10ms;
+--		action_imp	<= '1';	-- into counter start mode
+--		wait for 10ms;
+--		action_imp	<= '0';
+--
+--		wait for 10ms;
+--		action_imp	<= '1';	-- into counter stop mode
+--		wait for 10ms;
+--		action_imp	<= '0';
+--
+--		wait for 10ms;
+--		reset	<= '1';	--reset
+--		wait for 10ms;
+--		reset	<= '0';	
 
 -- test into stop watch freeze screen
 
